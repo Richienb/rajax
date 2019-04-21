9e28128b0e175a0982909cc897f364cb5cb1a945
Richie Bendall <richiebendall@gmail.com>
2019-04-21 14:13:40 +1200

    Update code

--- rajax/rajax.cs
+++ rajax/rajax.cs
@@ -57,19 +57,6 @@ namespace rajax
         {
             return RAJAX(uri, method: "POST", data: data);
         }
-
-        public static async Task<string> GetAsync(string uri)
-        {
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
-            request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
-
-            using (HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
-            using (Stream stream = response.GetResponseStream())
-            using (StreamReader reader = new StreamReader(stream))
-            {
-                return await reader.ReadToEndAsync();
-            }
-        }
     }
 }
 
